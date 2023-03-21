7f2c5b814c0b83370ea00b3c45b9facaee5ffdee
姜衿 <70458277@qq.com>
2023-03-21 14:01:27 +0800

    定义全局路由前缀。

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { middleWareAll } from './auth/auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1'); // 设置全局路由前缀
   app.use(middleWareAll);
   await app.listen(3200);
 }
